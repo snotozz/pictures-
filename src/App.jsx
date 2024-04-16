40afba74213f3b81506c3c3e2fab232477ce220d
snotozz <ej.gaitangarcia@gmail.com>
2024-04-16 15:31:44 -0700

    Changes on Search Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import SearchBar from './componentes/searchbar'
+import ImageList from './componentes/imagelist'
+import ImageShow from './componentes/imageshow'
 import './App.css'
 
 function App() {
-
+const [images, setImages]= useState([])
+const handleSubmit = async(term) => {
+  console.log('uste esta buscando con:', term)
+}
   return (
   
       <div>
-       <h1> hi </h1>
+       <SearchBar onSubmit= {handleSubmit} />
       </div>
       
   )
--- src/componentes/searchbar.jsx
+++ src/componentes/searchbar.jsx
@@ -1,7 +1,26 @@
-function SearchBar(){
+import { useState } from "react"
+
+function SearchBar({onSubmit}){
+    const [term,setTerm] = useState('')
+
+    const handleFormSubmit = (event)=> {
+        event.preventDefault()
+        console.log('Necesito decirle al componente padre sobre los datos')
+        onSubmit(term)
+    }
+
+    const handleChange =(event)=> {
+        console.log(event.target.value)
+        setTerm(event.target.value)
+    }
     return(
         <div>
-        <h1>Images</h1>
+        <h1>SearchBar</h1>
+        <form onSubmit={handleFormSubmit}>
+            Confirma tu busqueda:{term}
+            <input onChange={handleChange} value={term}/>
+        </form>
+        <button onClick={handleClick}>Click me !! </button>
         </div>
     )
 }
